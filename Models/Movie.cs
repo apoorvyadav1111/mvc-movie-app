b9d2880685ca0faaec5f063df88499c3c2712b8f
ayadav16 <ayadav7@binghamton.edu>
2024-05-26 18:28:49 -0400

    added code for crud using sqlite

---
+++ Models/Movie.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models;
+
+public class Movie
+{
+    public int Id {get; set;}
+
+    public string? Title {get; set;}
+
+    public string? Genre {get; set;}
+
+    public decimal Price {get; set;}
+
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate {get; set;}
+
+}
