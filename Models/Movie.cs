55c90542da89018f710ba98261d7bf781d3669ca
ayadav16 <ayadav7@binghamton.edu>
2024-05-26 19:51:34 -0400

    search using title

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models;
 
@@ -10,8 +11,10 @@ public class Movie
 
     public string? Genre {get; set;}
 
+    [Column(TypeName = "decimal(18, 2)")]
     public decimal Price {get; set;}
 
+    [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate {get; set;}
 
