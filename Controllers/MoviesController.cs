8eba7cd9311197cf27d1fc6545f9f43202a994c7
ayadav16 <ayadav7@binghamton.edu>
2024-05-27 23:13:11 -0400

    added filter based on genres

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -19,22 +19,38 @@ namespace MvcMovie.Controllers
             _context = context;
         }
 
-        // GET: Movies
-        public async Task<IActionResult> Index(string searchString)
+        // GET: Movies// GET: Movies
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
-            if(_context.Movie == null)
+            if (_context.Movie == null)
             {
-                return Problem("Entity set 'MvcMovieContext.Movie' is null");
+                return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
             }
 
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
             var movies = from m in _context.Movie
                         select m;
-            
-            if (!String.IsNullOrEmpty(searchString))
+
+            if (!string.IsNullOrEmpty(searchString))
             {
-                movies = movies.Where(s => s.Title.Contains(searchString));
+                movies = movies.Where(s => s.Title!.Contains(searchString));
             }
-            return View(await movies.ToListAsync());
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+
+            return View(movieGenreVM);
         }
 
         // GET: Movies/Details/5
